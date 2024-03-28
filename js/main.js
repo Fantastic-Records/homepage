835d373f10036be5a5484a66ebc9bc08af46bfc4
kudaken <kudaken.sub@gmail.com>
2024-03-28 18:41:09 +0900

    2024/03/28/ update

--- js/main.js
+++ js/main.js
@@ -121,3 +121,23 @@ $('#page-link a[href*="#"]').click(function () {//全てのページ内リンク
 	$('body,html').animate({scrollTop: pos}, 500); //取得した位置にスクロール。500の数値が大きくなるほどゆっくりスクロール
 	return false;
 });
+
+//初回のみモーダルをすぐ出す判定。flagがモーダル表示のstart_open後に代入される
+var access = $.cookie('access')
+if(!access){
+	flag = true;
+	$.cookie('access', false);
+}else{
+	flag = false	
+}
+
+//モーダル表示
+$(".modal-open").modaal({
+overlay_close:true,//モーダル背景クリック時に閉じるか
+before_open:function(){// モーダルが開く前に行う動作
+	$('html').css('overflow-y','hidden');/*縦スクロールバーを出さない*/
+},
+after_close:function(){// モーダルが閉じた後に行う動作
+	$('html').css('overflow-y','scroll');/*縦スクロールバーを出す*/
+}
+});
