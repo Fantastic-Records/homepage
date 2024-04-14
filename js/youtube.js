02e2e61af2c922ab388fbc68190e31237525e537
kudaken <kudaken.sub@gmail.com>
2024-04-14 18:02:41 +0900

    movieのページを修正

--- js/youtube.js
+++ js/youtube.js
@@ -1,5 +1,4 @@
-import { apiKey, channelId } from './config.js';
-
+// 手動で指定した動画IDのリスト
 const manualVideoIds = [
   'xlXq37Sp5po',
   '9_-_PgOlNik',
@@ -14,9 +13,10 @@ const manualVideoIds = [
 function displayThumbnails() {
   const thumbnailContainer = document.getElementById('thumbnail-container');
 
-  const videoIds = manualVideoIds;
+  // APIから取得した動画IDと手動で指定した動画IDのリストを組み合わせて重複を削除する
+  const allVideoIds = [...new Set([...manualVideoIds])]; 
 
-  videoIds.forEach(videoId => {
+  allVideoIds.forEach(videoId => {
     const thumbnailUrl = `https://img.youtube.com/vi/${videoId}/0.jpg`;
 
     const thumbnailElement = document.createElement('img');
@@ -29,57 +29,6 @@ function displayThumbnails() {
     });
 
     thumbnailContainer.appendChild(thumbnailElement);
-  });
-
-  fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=5&order=date&type=video&key=${apiKey}`)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`YouTube API request failed with status ${response.status}`);
-      }
-      return response.json();
-    })
-    .then(data => {
-      if (!data.items || data.items.length === 0) {
-        throw new Error('No videos found for the specified channel.');
-      }
-
-      const channelVideoIds = data.items.map(item => item.id.videoId);
-
-      channelVideoIds.forEach(videoId => {
-        if (!manualVideoIds.includes(videoId)) {
-          const thumbnailUrl = `https://img.youtube.com/vi/${videoId}/0.jpg`;
-
-          const thumbnailElement = document.createElement('img');
-          thumbnailElement.src = thumbnailUrl;
-          thumbnailElement.alt = `Thumbnail for video ${videoId}`;
-          thumbnailElement.className = 'thumbnail';
-
-          thumbnailElement.addEventListener('click', () => {
-            openModal(videoId);
-          });
-
-          thumbnailContainer.appendChild(thumbnailElement);
-        }
-      });
-    })
-    .catch(error => {
-      console.error('Error fetching video information:', error.message);
-    });
-}
-
-function openModal(videoId) {
-  const videoContainerBackground = document.getElementById('video-container-background');
-  const videoContainer = document.getElementById('video-container');
-  const videoPlayer = document.getElementById('video-player');
-
-  const embedUrl = `https://www.youtube.com/embed/${videoId}?autoplay=1`;
-
-  videoPlayer.src = embedUrl;
-  videoContainerBackground.style.display = 'flex';
-
-  videoContainerBackground.addEventListener('click', () => {
-    videoContainerBackground.style.display = 'none';
-    videoPlayer.src = '';
   });
 }
 
